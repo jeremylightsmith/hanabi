16082aa71bcae1b60057e2b898e6a1b44baff2da
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2017-09-16 15:28:49 -0700

    let's try again to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ if (production) {
     port: 5000,
     headers: { 'Access-Control-Allow-Origin': '*' }
   };
-  config.output.publicPath = '//localhost:5000/dist/';
+  config.output.publicPath = '//localhost:5000/hanabi/dist/';
   config.devtool = 'eval-source-map';
 }
 
